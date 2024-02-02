f5e0d5347a7c1cdda6048ee27646505a882a6ba0
PlusF <rkaneda896@gmail.com>
2024-02-02 14:27:01 +0900

    update update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,7 @@
 git pull origin main
 CALL venv\Scripts\activate
 python -m pip uninstall -y calibrator
+python -m pip uninstall -y dataloader
 python -m pip install git+https://github.com/PlusF/Calibrator
+python -m pip install git+https://github.com/PlusF/DataLoader
 pause
